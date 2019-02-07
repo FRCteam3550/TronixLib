b327cb7948bb421374fda3668f38862494174f4a
chien <jeanphilippe.blouin@gmail.com>
2019-02-07 08:25:21 -0500

    Ajouter le contenu de TuningPilotAxis

---
+++ src/main/java/TronixLib/TuningPilotAxis.java
@@ -0,0 +1,37 @@
+package TronixLib;
+
+public class TuningPilotAxis {
+
+    private int m_mode; // 0 : tel que
+                        // 1 : Au carre
+                        // 2 : exponentiel? etc 
+
+    public void TuningPilotAxis() {
+        m_mode = 0;
+    }
+
+    public void TuningPilotAxis(int tuningmode) {
+        m_mode = tuningmode;
+    }
+
+    public void setTuningMode(int newtuningmode) {
+        m_mode = newtuningmode;
+    }
+
+    public int getTuningMode() {
+        return m_mode;
+    }
+
+    public double getEnhancedAxis(double axisvalue) {
+        double finalvalue = 0;
+
+        if (m_mode == 0) {
+            finalvalue = axisvalue;
+        }
+        else if (m_mode == 1){
+            finalvalue =  Math.copySign(axisvalue * axisvalue, axisvalue);
+        }
+
+        return finalvalue;
+    }
+}
